ff041e0391760bc32ca2b1cfd2d09c995d74cc52
nidhaldring <dringthedreamer@gmail.com>
2023-01-24 02:48:09 +0100

    add day1 part2

--- day1/day1.go
+++ day1/day1.go
@@ -16,7 +16,7 @@ func readInput() string {
 	return string(data)
 }
 
-func main() {
+func part1() {
 	maxCal := 0
 	for _, elfCal := range strings.Split(readInput(), "\n\n") {
 		sum := 0
@@ -31,4 +31,36 @@ func main() {
 	}
 
 	fmt.Println(maxCal)
+}
+
+func part2() {
+	arr := [3]int{0, 0, 0}
+	for _, elfCal := range strings.Split(readInput(), "\n\n") {
+		sum := 0
+		for _, cal := range strings.Split(elfCal, "\n") {
+			n, _ := strconv.Atoi(cal)
+			sum += n
+		}
+
+		if sum > arr[0] {
+			arr[0], arr[1], arr[2] = sum, arr[0], arr[1]
+		} else if sum > arr[1] {
+			arr[1], arr[2] = sum, arr[1]
+		} else if sum > arr[2] {
+			arr[2] = sum
+		}
+	}
+
+	sum := 0
+	for _, v := range arr {
+		sum += v
+	}
+
+	fmt.Println(sum)
+
+}
+
+func main() {
+	part1()
+	part2()
 }

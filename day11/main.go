4006940447aa9dc71e3af81e90a8536cf7374ff7
nidhal rnine <nidhal.rnine@dnext.io>
2023-08-02 01:43:19 +0100

    add parsing

--- day11/main.go
+++ day11/main.go
@@ -9,9 +9,6 @@ import (
 	"strings"
 )
 
-type Op func(int) int
-type Test func(int) bool
-
 type Stack []int
 
 func NewStack() Stack {
@@ -32,34 +29,28 @@ func (s *Stack) IsEmpty() bool {
 	return len(*s) == 0
 }
 
+type Op func(int) int
+type Test func(int) bool
+
 type Monkey struct {
 	items     Stack
 	operation Op
 	test      Test
 
 	ml *MonkeysList
-	m1 int
-	m2 int
-}
-
-func (m *Monkey) SetItems(items string) {
-	parsedItems := NewStack()
-	for _, v := range strings.Split(items, ",") {
-		i, _ := strconv.Atoi(v)
-		parsedItems.Push(i)
-	}
-	m.items = parsedItems
+	m1 int // index of 1st monkey in the ml
+	m2 int // index of 2nd monkey in the ml
 }
 
 func (m *Monkey) StartThrowing() {
+	ml := m.ml
 	for !m.items.IsEmpty() {
-		top := m.items.Pop()
-		top = m.operation(top)
+		top := m.operation(m.items.Pop())
 		top = int(math.Round(float64(top) / 3))
 		if m.test(top) {
-			m.ml.Get(m.m1).items.Push(top)
+			ml.GetMonkey(m.m1).items.Push(top)
 		} else {
-			m.ml.Get(m.m2).items.Push(top)
+			ml.GetMonkey(m.m2).items.Push(top)
 		}
 	}
 }
@@ -81,11 +72,60 @@ func (ml *MonkeysList) CreateNewMonkey() *Monkey {
 	return m
 }
 
-func (ml *MonkeysList) Get(index int) *Monkey {
+func (ml *MonkeysList) GetMonkey(index int) *Monkey {
 	return ml.monkeys[index-1]
 }
 
-func part1(sc *bufio.Scanner) {
+func ApplyOperand(op string, x, y int) int {
+	switch op {
+	case "+":
+		return x + y
+	case "*":
+		return x * y
+	case "-":
+		return x - y
+	default:
+		return x / y
+	}
+}
+
+func ParseStartingItems(line string) []int {
+	var items string
+	fmt.Sscanf(line, "Starting Items: %s\n", &items)
+
+	parsedItems := NewStack()
+	for _, item := range strings.Split(items, ",") {
+		v, _ := strconv.Atoi(item)
+		parsedItems.Push(v)
+	}
+	return parsedItems
+}
+
+func ParseOperation(line string) Op {
+	var right, op string
+	fmt.Sscanf(line, "Operation: new = old %s %s\n", &op, &right)
+
+	return func(i int) int {
+		if right == "old" {
+			return ApplyOperand(op, i, i)
+		}
+
+		v, _ := strconv.Atoi(right)
+		return ApplyOperand(op, i, v)
+	}
+}
+
+func ParseTest(line string) Test {
+	var div int
+	fmt.Sscanf(line, "Test: divisible by %d", &div)
+
+	return func(i int) bool {
+		return i%div == 0
+	}
+
+}
+
+func ParseMonkeys(sc *bufio.Scanner) *MonkeysList {
 	ml := NewMonkeyList()
 	var currentMonkey *Monkey
 	for sc.Scan() {
@@ -93,16 +133,29 @@ func part1(sc *bufio.Scanner) {
 		if strings.Index(line, "Monkey") == 0 {
 			currentMonkey = ml.CreateNewMonkey()
 		} else if strings.Index(line, "Starting") == 0 {
-			var items string
-			fmt.Sscanf(line, "Starting Items: %s\n", &items)
-			currentMonkey.SetItems(items)
+			currentMonkey.items = ParseStartingItems(line)
 		} else if strings.Index(line, "Operation") == 0 {
-			var items string
-			fmt.Sscanf(line, "Starting Items: %s\n", &items)
-			currentMonkey.SetItems(items)
+			currentMonkey.operation = ParseOperation(line)
+		} else if strings.Index(line, "Test") == 0 {
+			currentMonkey.test = ParseTest(line)
+		} else if strings.Index(line, "If true") == 0 {
+			var i int
+			fmt.Sscanf(line, "If true: throw to monkey %d\n", &i)
+			currentMonkey.m1 = i
+		} else if strings.Index(line, "Test") == 0 {
+			var i int
+			fmt.Sscanf(line, "If false: throw to monkey %d\n", &i)
+			currentMonkey.m1 = i
 		}
 
 	}
+
+	return ml
+}
+
+func part1(sc *bufio.Scanner) {
+	ml := ParseMonkeys(sc)
+	fmt.Println(ml)
 }
 
 func part2(sc *bufio.Scanner) {}
